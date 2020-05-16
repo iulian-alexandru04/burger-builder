11a05bb632edf3e8386807773e102e4dd72f366f
iulian-alexandru04 <iulian_alexandru04@yahoo.com>
2020-05-16 18:41:33 +0300

    show message if burger is empty

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,13 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const expandedIngredientes = Object.keys(props.ingredients).map(
+    let expandedIngredientes = Object.keys(props.ingredients).map(
         ingredient => [...Array(props.ingredients[ingredient])].map((_, i) =>
-            <BurgerIngredient key={ingredient + i} type={ingredient} />
-        )
-    );
+                <BurgerIngredient key={ingredient + i} type={ingredient} />
+            )
+        ).reduce((arr, el) => arr.concat(el), []);
+    if (expandedIngredientes.length === 0)
+        expandedIngredientes = <p>Please start adding ingredients!</p>
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />
