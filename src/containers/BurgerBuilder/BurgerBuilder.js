6de1c6defbb1050ab4995c5315355557914b056e
iulian-alexandru04 <iulian_alexandru04@yahoo.com>
2020-05-20 22:30:31 +0300

    user should receive feedback for pending and successful order

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import axios from '../../axios-order';
 
 const INGREDIENT_PRICES = {
@@ -22,7 +23,8 @@ class BurgerBuilder extends Component {
         },
         totalPrice: 4,
         purchasable: false,
-        ordered: false
+        ordered: false,
+        loading: false
     }
 
     upsatePurchaseState = (crt_ingredients) => {
@@ -57,6 +59,7 @@ class BurgerBuilder extends Component {
     }
 
     continueOrderHandler = () => {
+        this.setState({loading: true});
         const order = {
             ingredients: this.state.ingredients,
             price: this.state.totalPrice,
@@ -69,22 +72,25 @@ class BurgerBuilder extends Component {
             deliveryMethod: 'fastest'
         }
         axios.post('/orders.json', order)
-            .then(response => console.log(response))
-            .catch(error => console.log(error));
+            .then(response => {this.setState({loading: false, ordered: false});})
+            .catch(error => {this.setState({loading: false, ordered: false});});
     }
 
     render () {
         const disabledInfo = {...this.state.ingredients};
         for(let ingr in disabledInfo)
             disabledInfo[ingr] = disabledInfo[ingr] <= 0;
+        let orderSummary = <OrderSummary
+            ingredients={this.state.ingredients}
+            price={this.state.totalPrice}
+            onCancel={this.cancelOrderHandler}
+            onContinue={this.continueOrderHandler} />;
+        if(this.state.loading)
+            orderSummary = <Spinner />;
         return (
             <Fragment>
                 <Modal show={this.state.ordered} onClose={this.cancelOrderHandler}>
-                    <OrderSummary 
-                        ingredients={this.state.ingredients}
-                        price={this.state.totalPrice}
-                        onCancel={this.cancelOrderHandler}
-                        onContinue={this.continueOrderHandler}/>
+                    {orderSummary}
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
