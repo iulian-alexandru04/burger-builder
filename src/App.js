2f283f32eb6e068237d6faae11979ee4b674f4ff
iulian-alexandru04 <iulian_alexandru04@yahoo.com>
2020-05-23 14:51:13 +0300

    add separate checkout page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Layout from './components/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+import {Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Layout>
-        <BurgerBuilder />
+        <Switch>
+          <Route path='/checkout' component={Checkout} />
+          <Route path='/' component={BurgerBuilder} />
+        </Switch>
+        
       </Layout>
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -62,21 +62,22 @@ class BurgerBuilder extends Component {
     }
 
     continueOrderHandler = () => {
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            address: {
-                street: 'Teststreet 1',
-                zipCode: '1234',
-                country: 'Germany'
-            },
-            email: 'test@test.com',
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => {this.setState({loading: false, ordered: false});})
-            .catch(error => {this.setState({loading: false, ordered: false});});
+        // this.setState({loading: true});
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     address: {
+        //         street: 'Teststreet 1',
+        //         zipCode: '1234',
+        //         country: 'Germany'
+        //     },
+        //     email: 'test@test.com',
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => {this.setState({loading: false, ordered: false});})
+        //     .catch(error => {this.setState({loading: false, ordered: false});});
+        this.props.history.push('/checkout');
     }
 
     render () {
