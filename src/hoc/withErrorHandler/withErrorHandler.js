9646354e8d93a624ccca1b9abefd2535834bb35a
iulian-alexandru04 <iulian_alexandru04@yahoo.com>
2020-06-13 11:42:44 +0300

    add custom hook

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,38 +1,20 @@
-import React, {Fragment, useState, useEffect} from 'react';
+import React, {Fragment} from 'react';
 import Modal from '../../components/UI/Modal/Modal';
+import useHttpErrorHandler from '../../hooks/http-error-handler';
 
 const withErrorHandler = (WrappedComponent, axios) => {
     return props => {
-        const [error, setError] = useState(null);
+        const [error, errorConfirmedHandler] = useHttpErrorHandler(axios);
 
-        const reqInterceptor = axios.interceptors.request.use(req => {
-            setError(null);
-            return req;
-        });
-        const resInterceptor = axios.interceptors.response.use(res => res, err => {
-            setError(err);
-        });
-
-        useEffect(() => {
-            return () => {
-                axios.interceptors.request.eject(reqInterceptor);
-                axios.interceptors.response.eject(resInterceptor);
-            };
-        }, [reqInterceptor, resInterceptor]);
-
-        const errorConfirmedHandler = () => {
-            setError(null);
-        }
-
-            return (
-                <Fragment>
-                    <Modal show={error} onClose={errorConfirmedHandler}>
-                        {error ? error.message : null}
-                    </Modal>
-                    <WrappedComponent {...props} />
-                </Fragment>
-            );
-        }
+        return (
+            <Fragment>
+                <Modal show={error} onClose={errorConfirmedHandler}>
+                    {error ? error.message : null}
+                </Modal>
+                <WrappedComponent {...props} />
+            </Fragment>
+        );
+    }
 }
 
 export default withErrorHandler;
